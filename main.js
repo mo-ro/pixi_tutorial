4de03303cf57d7dba3cd1299847378f6211e675b
mo-ro <gainmoro@gmail.com>
2018-10-16 14:08:43 -0700

    add sprite

--- main.js
+++ main.js
@@ -10,4 +10,18 @@ app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
 app.renderer.resize(window.innerWidth, window.innerHeight);
 
+PIXI.loader
+  .add("egg.png")
+  .load(setup);
+
+function setup() {
+  let sprite = new PIXI.Sprite(
+    PIXI.loader.resources["egg.png"].texture
+  )
+
+app.stage.addChild(sprite)
+
+}
+
+
 document.body.appendChild(app.view);
